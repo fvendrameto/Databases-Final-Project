52e9139ea679bec5ad66347ba1ce2249209f513d
Gustavo <gsutter98@gmail.com>
2018-06-22 22:58:02 -0300

    Db helper pronto

--- database/dbHelper.py
+++ database/dbHelper.py
@@ -21,7 +21,7 @@ class dbHelper():
 			elif isinstance(value, int):
 				values[i] = str(value)
 			elif isinstance(value, datetime.datetime):
-				values[i] = "%2d/%02d/%4d" % (value.day, value.month, value.year)
+				values[i] = "'%2d/%02d/%4d'" % (value.day, value.month, value.year)
 		return values
 
 	def _run_command(self, cmd):
@@ -62,4 +62,116 @@ class dbHelper():
 		self._run_command(cmd)
 
 
-	
+	def _run_select(self, cmd, debug = True):
+		cursor = self.connection.cursor()
+		cursor.execute(cmd)
+
+		result = []
+		for elem in cursor:
+			result.append(elem)
+
+		cursor.close()
+
+		if debug:
+			for r in result:
+				print(r)
+
+		return result
+
+	def getAllAniversarios(self):
+		cmd = "SELECT F.CLIENTE, F.DATA, F.NUMERO_CONVIDADOS, F.PRECO, F.GERENTE, F.CASA_FESTA, A.NOME_ANIVERSARIANTE, A.TEMA, A.FAIXA_ETARIA, COUNT(GF.GARCOM)\
+			FROM FESTA F JOIN ANIVERSARIO A ON A.CLIENTE = F.CLIENTE AND A.DATA = F.DATA\
+			LEFT JOIN GARCOM_FESTA GF ON (GF.DATA = A.DATA AND GF.CLIENTE = A.CLIENTE)\
+			GROUP BY(F.CLIENTE, F.DATA, F.NUMERO_CONVIDADOS, F.PRECO, F.GERENTE, F.CASA_FESTA, A.NOME_ANIVERSARIANTE, A.TEMA, A.FAIXA_ETARIA)"
+
+		return self._run_select(cmd)
+
+	def getGarconsFesta(self, cliente, data):
+		cliente, data = self._preprocess_values([cliente, data])
+		cmd = "SELECT FU.NOME, GF.GARCOM AS CPF, FU.COMISSAO FROM\
+			FESTA F JOIN GARCOM_FESTA GF ON (F.CLIENTE = GF.CLIENTE AND F.DATA = GF.DATA)\
+			AND (F.CLIENTE = " + cliente + " AND F.DATA = TO_DATE(" + data + ", 'DD/MM/YYYY'))\
+			JOIN FUNCIONARIO FU ON FU.CPF = GF.GARCOM"
+
+		return self._run_select(cmd)
+
+	def getBarracasAniversario(self, cliente, data):
+		cliente, data = self._preprocess_values([cliente, data])
+		cmd = "SELECT B.NUMERO, FU.NOME, B.OPERADOR AS CPF, FU.COMISSAO FROM\
+			FESTA F JOIN BARRACA_RASPADINHA B ON (F.CLIENTE = B.CLIENTE AND F.DATA = B.DATA)\
+			AND (F.CLIENTE = " + cliente + " AND F.DATA = TO_DATE(" + data + ", 'DD/MM/YYYY'))\
+			JOIN FUNCIONARIO FU ON FU.CPF = B.OPERADOR"
+
+		return self._run_select(cmd)
+
+	def getEnderecoFesta(self, cliente, data):
+		cliente, data = self._preprocess_values([cliente, data])
+		cmd = "SELECT E.RUA, E.NUMERO, E.CEP, E.CIDADE, E.ESTADO FROM\
+		FESTA F JOIN CASA_FESTA CF ON (F.CASA_FESTA = CF.NOME)\
+		AND (F.CLIENTE = " + cliente + " AND F.DATA = TO_DATE(" + data + ", 'DD/MM/YYYY'))\
+		JOIN ENDERECO E ON E.ID = CF.ENDERECO"
+
+		return self._run_select(cmd)
+
+	def getGarconsLivres(self, data):
+		data = self._preprocess_values([data])[0]
+		cmd = "SELECT F.NOME, F.CPF, F.COMISSAO FROM FUNCIONARIO F WHERE UPPER(F.CARGO) = 'GARCOM' AND\
+			F.CPF NOT IN(\
+			SELECT GF.GARCOM FROM GARCOM_FESTA GF WHERE GF.DATA = TO_DATE(" + data + ", 'DD/MM/YYYY'))"
+
+		return self._run_select(cmd)
+
+	def getOperadoresLivres(self, data):
+		data = self._preprocess_values([data])[0]
+		cmd = "SELECT F.NOME, F.CPF, F.COMISSAO FROM FUNCIONARIO F WHERE UPPER(F.CARGO) = 'OPERADOR' AND\
+			F.CPF NOT IN(\
+			SELECT BR.OPERADOR FROM BARRACA_RASPADINHA BR WHERE BR.DATA = TO_DATE(" + data + ", 'DD/MM/YYYY'))"
+		
+		return self._run_select(cmd)
+
+	def getGerentesLivres(self, data):
+		data = self._preprocess_values([data])[0]
+		cmd = "SELECT F.NOME, F.CPF, F.COMISSAO FROM FUNCIONARIO F WHERE UPPER(F.CARGO) = 'GERENTE' AND\
+			F.CPF NOT IN(\
+			SELECT FE.GERENTE FROM FESTA FE WHERE FE.DATA = TO_DATE(" + data + ", 'DD/MM/YYYY'))"
+		
+		return self._run_select(cmd)
+
+	def getAllCasasFesta(self):
+		cmd = "SELECT NOME FROM CASA_FESTA"
+		return self._run_select(cmd)
+		
+
+	def getAllGerentes(self):
+		cmd = "SELECT F.CPF, F.NOME, F.TEL_MOVEL, F.TEL_FIXO, F.COMISSAO, MIN(FE.DATA) AS DATA_PROXIMA_FESTA\
+			FROM FUNCIONARIO F LEFT JOIN FESTA FE ON FE.GERENTE = F.CPF WHERE UPPER(F.CARGO) = 'GERENTE'\
+			GROUP BY(F.CPF, F.NOME, F.TEL_MOVEL,  F.TEL_FIXO, F.COMISSAO)"
+		return self._run_select(cmd)
+
+	def getAllOperadores(self):
+		cmd = "SELECT F.CPF, F.NOME, F.TEL_MOVEL, F.TEL_FIXO, F.COMISSAO, MIN(BR.DATA) AS DATA_PROXIMA_FESTA \
+				FROM FUNCIONARIO F LEFT JOIN BARRACA_RASPADINHA BR ON BR.OPERADOR = F.CPF WHERE UPPER(F.CARGO) = 'OPERADOR'\
+				GROUP BY(F.CPF, F.NOME, F.TEL_MOVEL,  F.TEL_FIXO, F.COMISSAO)"
+		return self._run_select(cmd)
+
+	def getAllGarcons(self):
+		cmd = "SELECT F.CPF, F.NOME, F.TEL_MOVEL, F.TEL_FIXO, F.COMISSAO, MIN(GF.DATA) AS DATA_PROXIMA_FESTA\
+			FROM FUNCIONARIO F LEFT JOIN GARCOM_FESTA GF ON GF.GARCOM = F.CPF WHERE UPPER(F.CARGO) = 'GARCOM'\
+			GROUP BY(F.CPF, F.NOME, F.TEL_MOVEL,  F.TEL_FIXO, F.COMISSAO)"
+		return self._run_select(cmd)
+
+	def getBebidasInInterval(self, min_value, max_value):
+		cmd = "SELECT B.NOME, B.VOLUME, B.QUANTIDADE, B.PRECO FROM BEBIDA B WHERE B.QUANTIDADE BETWEEN " + str(min_value) + " AND " + str(max_value)
+		return self._run_select(cmd)
+
+
+	def getAllFornecedores(self):
+		cmd = "SELECT F.CNPJ, F.NOME, F.TELEFONE, D.BANCO, D.AGENCIA, D.CONTA, D.TIPO_CONTA FROM FORNECEDOR F JOIN DADOS_BANCARIOS D ON F.DADOS_BANCARIOS = D.ID"
+		return self._run_select(cmd)
+
+	def getAllClientes(self):
+		cmd = "SELECT C.CPF, C.NOME, C.TEL_FIXO, C.TEL_MOVEL, D.BANCO, D.AGENCIA, D.CONTA, D.TIPO_CONTA, COUNT(*) AS NUMERO_FESTAS\
+			FROM CLIENTE C JOIN DADOS_BANCARIOS D ON C.DADOS_BANCARIOS = D.ID\
+			LEFT JOIN FESTA F ON C.CPF = F.CLIENTE\
+			GROUP BY(C.CPF, C.NOME, C.TEL_FIXO, C.TEL_MOVEL, D.BANCO, D.AGENCIA, D.CONTA, D.TIPO_CONTA)"
+		return self._run_select(cmd)
